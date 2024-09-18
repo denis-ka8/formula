fcfa09847032aa0a78a163a4ed242be4f9f183fe
denis-ka8 <denkazancev8@gmail.com>
2024-09-18 17:08:23 +0300

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "./store";
+
+async function enableMocking() {
+	if (process.env.NODE_ENV === "development") {
+		const { worker } = await import("./mock/node");
+		return worker.start();
+	}
+}
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+enableMocking().then(() => {
+	root.render(
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
+	);
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/mock/handlers.ts
@@ -0,0 +1,6 @@
+import { http, HttpResponse } from "msw";
+// import { dateIntervals } from "./dateIntervals";
+
+export const handlers = [
+	// http.get(`/api/`, () => HttpResponse.json(dateIntervals)),
+];
